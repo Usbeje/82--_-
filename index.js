62f0ae9d63e5089e989457acc8dc067f0bdd41e9
Usbeje <lhi159712@gmail.com>
2024-07-10 14:23:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const qrCodeReader = require('./features/qrCodeReader');
 const youtubeDownload = require('./features/youtubeDownload');
 const morseCode = require('./features/morseCode');
 const cekKiamat = require('./features/cekKiamat');
-require('dotenv').config();
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+// Ganti dengan token bot Telegram Anda
+const TELEGRAM_BOT_TOKEN = '7323908580:AAEJRXUBNDaVNUHK-6XmOr7ycLG65fqq1X8';
+
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
 
 bot.start((ctx) => {
   ctx.reply(
-    'Selamat datang! Pilih fitur yang ingin digunakan:',
+    'Selamat datang! Pilih fitur yang ingin Anda gunakan:',
     Markup.inlineKeyboard([
       [Markup.button.callback('Kode QR', 'start_qr')],
       [Markup.button.callback('Download YouTube', 'start_download')],
