f191e9c9e8f20e7f6d5a1a629d0dea2ed4b318a4
Usbeje <lhi159712@gmail.com>
2024-07-10 18:41:54 +0700

    Update morseCode.js

--- Features/morseCode.js
+++ Features/morseCode.js
@@ -9,41 +9,27 @@ const morseDict = {
   '0': '–––––', ' ': '/'
 };
 
-const reverseMorseDict = {};
-for (let key in morseDict) {
-  reverseMorseDict[morseDict[key]] = key;
+function encodeToMorse(text) {
+  return text.toLowerCase().split('').map(char => morseDict[char] || '').join(' ');
 }
 
-const textToMorse = (text) => {
-  return text.toLowerCase().split('').map(char => {
-    return morseDict[char] || char; // Jika karakter tidak ada di kamus, biarkan apa adanya
-  }).join(' ');
-};
-
-const morseToText = (morse) => {
-  return morse.split(' ').map(code => {
-    return reverseMorseDict[code] || code; // Jika kode Morse tidak ada di kamus, biarkan apa adanya
-  }).join('');
-};
+function decodeFromMorse(morseCode) {
+  const reversedMorseDict = Object.fromEntries(Object.entries(morseDict).map(([k, v]) => [v, k]));
+  return morseCode.split(' ').map(code => reversedMorseDict[code] || '').join('');
+}
 
 module.exports = (bot) => {
-  bot.action('start_morse', (ctx) => {
-    ctx.reply('Kirim teks yang ingin dikonversi ke Morse atau kirim kode Morse yang ingin dikonversi ke teks. Gunakan format:\n/morse [teks]\n/text [kode Morse]');
-  });
-
   bot.onText(/\/morse (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
-    const text = match[1];
-
-    let morse = textToMorse(text);
-    bot.sendMessage(chatId, `Kode morse:\n\n${morse}`);
+    const textToConvert = match[1];
+    const morseCode = encodeToMorse(textToConvert);
+    bot.sendMessage(chatId, `Kode Morse dari "${textToConvert}" adalah:\n${morseCode}`);
   });
 
   bot.onText(/\/text (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
-    const morse = match[1];
-
-    let text = morseToText(morse);
-    bot.sendMessage(chatId, `Teks:\n\n${text}`);
+    const morseToConvert = match[1];
+    const plainText = decodeFromMorse(morseToConvert);
+    bot.sendMessage(chatId, `Teks dari kode Morse "${morseToConvert}" adalah:\n${plainText}`);
   });
 };
