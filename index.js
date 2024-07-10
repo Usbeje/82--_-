5aaeef97543e1d83152115542d3c199051ff89d8
Usbeje <lhi159712@gmail.com>
2024-07-10 17:50:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-const { Telegraf, Markup } = require('telegraf');
-const qrCodeReader = require('./Features/qrCodeReader');
-const youtubeDownload = require('./Features/youtubeDownload');
-const morseCode = require('./Features/morseCode');
-const cekKiamat = require('./Features/cekKiamat');
+const TelegramBot = require('node-telegram-bot-api');
+const morseCode = require('./features/morseCode');
+const qrCodeReader = require('./features/qrCodeReader');
+const youtubeDownload = require('./features/youtubeDownload');
 
-// Ganti dengan token bot Telegram Anda
-const TELEGRAM_BOT_TOKEN = '7323908580:AAEJRXUBNDaVNUHK-6XmOr7ycLG65fqq1X8';
+// Masukkan token bot Anda di sini
+const token = '7323908580:AAEJRXUBNDaVNUHK-6XmOr7ycLG65fqq1X8';
 
-const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+// Buat instance bot
+const bot = new TelegramBot(token, { polling: true });
 
-bot.start((ctx) => {
-  ctx.reply(
-    'Selamat datang! Pilih fitur yang ingin Anda gunakan:',
-    Markup.inlineKeyboard([
-      [Markup.button.callback('Kode QR', 'start_qr')],
-      [Markup.button.callback('Download YouTube', 'start_download')],
-      [Markup.button.callback('Kode Morse', 'start_morse')],
-      [Markup.button.callback('Cek Kiamat', 'start_kiamat')]
-    ])
-  );
-});
-
+// Gunakan fitur
+morseCode(bot);
 qrCodeReader(bot);
 youtubeDownload(bot);
-morseCode(bot);
-cekKiamat(bot);
 
-bot.launch();
+console.log('Bot sedang berjalan...');
